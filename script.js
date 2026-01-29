db725bf45cf04ad72671b0bc1ef6171377ce1fbe
Brahmani.V <brahmanibrahmani36@gmail.com>
2026-01-29 13:46:49 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Function for "View on GitHub" button
+function openGitHub() {
+    // Replace the URL with your actual GitHub repository later
+    window.open("https://github.com/", "_blank");
+}
+
+// Function for "Learn More" button
+function learnMore() {
+    alert("RepliCode helps you learn coding by building real-world projects.");
+}
